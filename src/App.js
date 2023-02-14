51ff8d8b18729504faf63211910012315d14b2ca
Renzo Lacovara <rnlacovara@gmail.com>
2023-02-13 23:37:55 -0300

    main added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Header from "./Componentes/Header";
 // import Main from "./Componentes/Main";
 import Footer from "./Componentes/Footer";
 import "./Styles/App.scss";
-import Home from "./Componentes/Home";
-import Nosotros from './Componentes/Nosotros'
-import Servicios from './Componentes/Servicios'
-import Proveedores from './Componentes/Proveedores'
-import Contacto from './Componentes/Contacto'
+import Main from "./Componentes/Main";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/nosotros" element={<Nosotros/>}/>
-        <Route path="/servicios" element={<Servicios/>}/>
-        <Route path="/proveedores" element={<Proveedores/>}/>
-        <Route path="/contacto" element={<Contacto/>}/>
-      </Routes>
-     
+      <Main />
+
       <Footer />
     </BrowserRouter>
   );
--- src/Componentes/Main.js
+++ src/Componentes/Main.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Home";
+import Nosotros from "./Nosotros";
+import Servicios from "./Servicios";
+import Proveedores from "./Proveedores";
+import Contacto from "./Contacto";
 
 const Main = () => {
   return (
@@ -8,6 +12,10 @@ const Main = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:Home" element={<Home />} />
+        <Route path="/nosotros" element={<Nosotros />} />
+        <Route path="/servicios" element={<Servicios />} />
+        <Route path="/proveedores" element={<Proveedores />} />
+        <Route path="/contacto" element={<Contacto />} />
         <Route
           path="*"
           element={<h1 className="h-screen">Oops, something went wrong</h1>}
