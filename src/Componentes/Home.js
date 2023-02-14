d8e4050fc903d3d4445b888f8df2e09481bd8b51
Renzo Lacovara <rnlacovara@gmail.com>
2023-02-14 14:42:23 -0300

    detalles al home

--- src/Componentes/Home.js
+++ src/Componentes/Home.js
@@ -4,13 +4,15 @@ const Home = () => {
   return (
     <div class="container-home">
       <div class="banner">
-        <div class="banner-presentacion">
-          <h2>PRO SALUD MEDICAL</h2>
-          <h3>El programa de asistencias mas completo de Mexico</h3>
-          <p>¿Quieres brindarle a tus empleados los mejores beneficios?</p>
-          <button class="banner-button">
-            <a href=".">Conocer servicios</a>
-          </button>
+        <div class="banner-bg">
+          <div class="banner-presentacion">
+            <h2>PRO SALUD MEDICAL</h2>
+            <h3>El programa de asistencias mas completo de Mexico</h3>
+            <p>¿Quieres brindarle a tus empleados los mejores beneficios?</p>
+            <button class="banner-button">
+              <a href=".">Conocer servicios</a>
+            </button>
+          </div>
         </div>
       </div>
       <div class="div-presentacion">
