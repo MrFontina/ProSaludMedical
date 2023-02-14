a2b09c32b6f1fa512d90e51e1f7bbffe178e20e3
Renzo Lacovara <rnlacovara@gmail.com>
2023-02-13 21:21:04 -0300

    agrega Home

--- src/Componentes/Main.js
+++ src/Componentes/Main.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
 
 const Main = () => {
-  return <main>Main</main>;
+  return (
+    <main>
+      <Routes>
+        <Route path="/:Home" element={<Home />} />
+        <Route
+          path="*"
+          element={<h1 className="h-screen">Oops, something went wrong</h1>}
+        />
+      </Routes>
+    </main>
+  );
 };
 
 export default Main;
