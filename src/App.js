97874c7b3c14bd8fa1b56cfb811e60049494e05a
Nahuel Bracchitta <nahuelbracchitta@gmail.com>
2023-02-13 22:57:51 -0300

    navbar comienza

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./Componentes/Header";
-import Main from "./Componentes/Main";
+// import Main from "./Componentes/Main";
 import Footer from "./Componentes/Footer";
 import "./Styles/App.scss";
+import Home from "./Componentes/Home";
+import Nosotros from './Componentes/Nosotros'
+import Servicios from './Componentes/Servicios'
+import Proveedores from './Componentes/Proveedores'
+import Contacto from './Componentes/Contacto'
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Main />
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/nosotros" element={<Nosotros/>}/>
+        <Route path="/servicios" element={<Servicios/>}/>
+        <Route path="/proveedores" element={<Proveedores/>}/>
+        <Route path="/contacto" element={<Contacto/>}/>
+      </Routes>
+     
       <Footer />
     </BrowserRouter>
   );
--- src/Componentes/Header.js
+++ src/Componentes/Header.js
@@ -1,10 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header>
-      <img class="logo" src="../../Images/logo2.png" alt="logo" />
-      <h1>Pro Salud Medical</h1>
+      <div className="navLeft">
+        <Link to='/'>
+        <img class="logo" src="../../Images/logo2.png" alt="logo" />
+        
+        </Link>
+        <h1>Pro Salud Medical</h1>
+      
+      </div>
+      <div className="navRight">
+        <div className="links">
+          <Link to='/nosotros' className="navLink">Nosotros</Link>
+          <Link to='/servicios' className="navLink">Servicios</Link>
+          <Link to='/proveedores' className="navLink">¿Quieres ser proovedor?</Link>
+          <Link to='/contacto' className="navLink">Contacto</Link>
+        </div>
+      </div>
+      
     </header>
   );
 };
