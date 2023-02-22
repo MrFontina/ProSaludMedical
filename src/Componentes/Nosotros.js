f01df62122e62ca5f6e9babe1740fae6120c9fc9
Nahuel Bracchitta <nahuelbracchitta@gmail.com>
2023-02-22 15:27:08 -0300

    nosotros funcionando

--- src/Componentes/Nosotros.js
+++ src/Componentes/Nosotros.js
@@ -1,7 +1,40 @@
 import React from "react";
 
+
 const Nosotros = () => {
-    return <h1>Nosotros</h1>;
-  };
+  return (
+    
+      <div className="contenedor-nosotros">
+        <div className="nosotros-left">
+          <div className="nosotros-card">
+            <img src="../../Images/flecha.png" alt=""></img>
+            <div className="card-content">
+              <h5>Misión</h5>
+              <p>Crear un valor agregado a nuestros clientes y socios comerciales ofreciendo soluciones médicas integrales e innovadoras a precios accesibles.</p>
+            </div>
+          </div>
+          <div className="nosotros-card">
+            <img src="../../Images/ojo.png" alt=""></img>
+            <div className="card-content">
+              <h5>Visión</h5>
+              <p>Lograr un posicionamiento top en el mercado de servicios de asistencias y salud a través de una red de profesionales comprometidos a brindar la mejor experiencia a nuestros clientes con tecnología y expertise.</p>
+            </div>
+          </div>
+          <div className="nosotros-card">
+            <img src="../../Images/valores.png" alt=""></img>
+            <div className="card-content">
+              <h5>Valores</h5>
+              <p>Accesibilidad - Excelencia -Colaboración Responsabilidad - Innovación - Confianza</p>
+            </div>
+          </div>
+        </div>
+        <div className="nosotros-right">
+          <img src="../../Images/medicorecortado.png" alt=""></img>
+        </div>
+      </div>
+    
+
+  )
+};
 
 export default Nosotros
