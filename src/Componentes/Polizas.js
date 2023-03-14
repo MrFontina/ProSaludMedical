a3be895aa10c1504bb6f36ccf5905568c3e5b92a
Nahuel Bracchitta <nahuelbracchitta@gmail.com>
2023-03-13 23:55:38 -0300

    accidentes

--- src/Componentes/Polizas.js
+++ src/Componentes/Polizas.js
@@ -1,12 +1,27 @@
 import React from "react";
+import ComponenteAsistencias from "./ComponenteAsistencias";
 
 const Polizas = () =>{
     return(
         <>
-        <div className="titleMembresia">
-                <h4>Membresía / Póliza de accidentes</h4>
-                <h2>Póliza de accidentes</h2>
-            </div>
+        <ComponenteAsistencias
+        img={{ backgroundImage: `url(${process.env.PUBLIC_URL
+            + "/Images/polizaaccidentes.webp"})`}}
+            title="Póliza de accidentes"
+            subtitle="Membresía / Poliza de accidentes"
+        />
+        <article className='articleAsistencias'>
+            <p>
+            Esta póliza tiene un costo de $50 pesos extra al adquirir la membresia.
+            </p>
+            <p>
+            Solo para titular:
+            </p>
+            <ul>
+                <li>Muerte accidental de $100,000</li>
+                <li>Pago directo o rembolso de GM por accidente $25,000 deducible $0</li>
+            </ul>
+        </article>
         </>
     )
 }
