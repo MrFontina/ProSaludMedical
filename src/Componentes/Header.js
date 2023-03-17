5d02c5ca4c67eedec626bbe6c8feac216d0afd6e
Renzo Lacovara <rnlacovara@gmail.com>
2023-03-17 01:16:15 -0300

    navbar links added

--- src/Componentes/Header.js
+++ src/Componentes/Header.js
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Dropdown from "react-bootstrap/Dropdown";
 import Button from "react-bootstrap/Button";
+import { IoIosArrowDropdownCircle } from "react-icons/io";
 
 const Header = () => {
   const [burga, setburga] = useState(false);
@@ -15,6 +17,7 @@ const Header = () => {
       setburga(true);
     }
   };
+
   return (
     <Navbar collapseOnSelect expand="md" className="navBar-cont">
       <Container fluid className="cont">
@@ -43,9 +46,40 @@ const Header = () => {
             <Link to="/nosotros" className="navLink">
               Nosotros
             </Link>
-            <Link to="/servicios" className="navLink">
-              Servicios
-            </Link>
+            <Dropdown as={ButtonGroup} class="dropdown-div">
+              <Link to="/servicios" className="navLink link2">
+                Servicios
+              </Link>
+              <Dropdown.Toggle split id="dropdown-split-center">
+                <IoIosArrowDropdownCircle className="dropdown-btn" />
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu class="drop-menu">
+                <Dropdown.Item id="ditem">
+                  <Link to="/servicios/salud" class="drop-link">
+                    Salud
+                  </Link>{" "}
+                </Dropdown.Item>
+                <Dropdown.Item>
+                  {" "}
+                  <Link to="/servicios/asistencias" class="drop-link">
+                    Asistencias
+                  </Link>
+                </Dropdown.Item>
+                <Dropdown.Item>
+                  {" "}
+                  <Link to="/servicios/polizas" class="drop-link">
+                    Polizas de accidentes
+                  </Link>
+                </Dropdown.Item>
+                <Dropdown.Item>
+                  {" "}
+                  <Link to="/servicios/beneficios" class="drop-link">
+                    Beneficios
+                  </Link>
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
             <Link to="/proveedores" className="navLink prove">
               <Button variant="success" className="botonProveedor">
                 ¿Quieres ser proveedor?
